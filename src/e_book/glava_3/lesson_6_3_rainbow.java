044043f66b64010b5d9e38d048c8f8eddd657613
number17 <number1788@gmail.com>
2014-10-09 16:02:10 +0700

    1.02.2
    тест

--- src/e_book/glava_3/lesson_6_3_rainbow.java
+++ src/e_book/glava_3/lesson_6_3_rainbow.java
@@ -4,5 +4,13 @@ package e_book.glava_3;
  * Created by Alexandr on 07.10.2014.
  */
 public class lesson_6_3_rainbow {
-    int i;
+    public static void main(String[]args){
+        System.out.print("Красный");
+        System.out.print("Оранжевый");
+        System.out.println("Желтый");
+        System.out.print("Зеленый");
+        System.out.print("Голубой");
+        System.out.println("Сининий");
+        System.out.print("Фиолетовый");
+    }
 }
