a364ab4380672ed31921ea89af7e8823c494de5e
number17 <number1788@gmail.com>
2014-10-24 17:30:59 +0700

    1.05.2
    ebook 4 glava full

--- src/books/glava_5/this_tut.java
+++ src/books/glava_5/this_tut.java
@@ -6,8 +6,34 @@ package books.glava_5;
 public class this_tut {
     int petalCount=0;
     String s = "initinal value";
+
     this_tut(int petals){
         petalCount = petals;
         System.out.println("Constructor w/ int arg only, petalCount= "  + petalCount);
+    }
+
+    this_tut(String ss){
+        System.out.println("Constructor w/ String arg only, s = " + ss);
+        s = ss;
+    }
+
+    this_tut(String s, int petals){
+        this(petals);
+        this.s=s;
+        System.out.println("String & int args");
+    }
+
+    this_tut(){
+        this("hi",47);
+        System.out.println("default constructor");
+    }
+
+    void printPetalCount(){
+        System.out.println("petalCount = " + petalCount + " s = " + s);
+    }
+
+    public static void main(String[] args){
+        this_tut thisTut = new this_tut();
+        thisTut.printPetalCount();
     }
 }
