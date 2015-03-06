6b97c83e3caef138ed49bc2f08e6b882029b4071
number17 <number1788@gmail.com>
2015-03-06 14:00:46 +0700

    1.05
    ebook 4 glava full

--- src/e_book/glava_5/lesson_9_1_number.java
+++ src/e_book/glava_5/lesson_9_1_number.java
@@ -6,14 +6,29 @@ import java.util.Scanner;
  * Created by Alexandr on 04.03.2015.
  */
 public class lesson_9_1_number {
+
     static int number(){
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Vvedite kolijestvo");
         int i = scanner.nextInt();
         return i;
     }
 
+    static int[] aray_number(int i){
+        Scanner scanner = new Scanner(System.in);
+        int[] array;
+        array = new int[i];
+        for(int j=0;j<i;j++) {
+            System.out.println("Vvedite chislo " + j);
+            array[j] = scanner.nextInt();
+        }
+        return array;
+    }
+
     public static void main(String[] args){
-        System.out.print(number());
+
+        int[] array1 = aray_number(number());
+
 
     }
 }
